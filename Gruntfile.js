f9923fb9048589a34e8eff2f73ed690da04494e9
Jonah Joselow <jonahjoselow@gmail.com>
2016-03-28 22:39:45 -0400

    Added Grunt + pkg

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,63 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    less: {
-        run: {
-            files: {
-                "build/style.css": "css/style.less"
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+    grunt.initConfig({
+        less: {
+            run: {
+                files: {
+                    "build/style.css": "css/style.less"
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'js',
+                    src: '**',
+                    dest: 'build/js'
+                }, {
+                    expand: true,
+                    cwd: 'html',
+                    src: '**',
+                    dest: 'build/'
+                }, ]
+            }
+        },
+        watch: {
+            less: {
+                files: ['css/**/*.less'],
+                tasks: ['less']
+            },
+            default: {
+                files: ['css/**/*.less', 'js/**/*.js', 'html/**/*.html'],
+                tasks: ['less:run', 'copy:main']
+            },
+            options: {
+                livereload: true,
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    hostname: "0.0.0.0",
+                    base: 'build',
+                    livereload: true,
+                }
+            }
+        },
+        open: {
+            all: {
+                path: 'http://localhost:<%= connect.server.options.port%>'
             }
         }
-    },
-    copy: {
-        main: {
-            files: [
-                {expand: true, cwd: 'js', src: '**', dest: 'build/js'},
-                {expand: true, cwd: 'html', src: '**', dest: 'build/'},
-            ]
-        }
-    },
-    watch: {
-        less: {
-            files: ['css/**/*.less'],
-            tasks: ['less']
-        },
-        default: {
-            files: ['css/**/*.less', 'js/**/*.js', 'html/**/*.html'],
-            tasks: ['less:run', 'copy:main']
-        }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['less:run', 'copy:main', 'watch:default']);
+    grunt.registerTask('default', ['less:run', 'copy:main',  'connect:server', 'open', 'watch:default']);
+
 
 };
